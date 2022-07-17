685331f68aecce68b8817d0418de8308111afb54
Timothy Karani <kartimothy@gmail.com>
2022-07-17 17:52:34 +0300

    Tested underscore name generator

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class DemoUtilsTest {
     DemoUtils demoUtils;
 
@@ -14,15 +15,15 @@ class DemoUtilsTest {
     }
 
     @Test
-    @DisplayName("Equals and Not Equals")
-    void testEqualsAndNotEquals() {
+    // @DisplayName("Equals and Not Equals")
+    void test_Equals_And_Not_Equals() {
         assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
         assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
     }
 
     @Test
-    @DisplayName("Null and Not Null")
-    void testNullAndNotNull() {
+    // @DisplayName("Null and Not Null")
+    void test_Null_And_Not_Null() {
         String str1 = null;
         String str2 = "luv2code";
 
