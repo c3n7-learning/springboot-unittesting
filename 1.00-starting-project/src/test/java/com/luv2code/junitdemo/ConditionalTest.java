655bdeeddccea905568e9def9dc266bec343c258
Timothy Karani <kartimothy@gmail.com>
2022-07-19 00:21:58 +0300

    Testing for enableed on OS

---
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/ConditionalTest.java
@@ -0,0 +1,40 @@
+package com.luv2code.junitdemo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+class ConditionalTest {
+
+    @Test
+    @Disabled("Don't run unit JIRA #123 is resolved")
+    void basicTest() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void testForWindowsOnly() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    void testForLinuxOnly() {
+        // execute method and perform asserts
+    }
+
+
+    @Test
+    @EnabledOnOs(OS.MAC)
+    void testForMacOnly() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs({OS.LINUX, OS.WINDOWS})
+    void testForLinuxAndWindowsOnly() {
+        // execute method and perform asserts
+    }
+}
