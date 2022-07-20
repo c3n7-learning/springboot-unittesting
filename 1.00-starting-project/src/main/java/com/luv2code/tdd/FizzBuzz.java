0c6147e131a26e28022cc99ed99cc07f93892cb7
Timothy Karani <kartimothy@gmail.com>
2022-07-20 21:41:04 +0300

    Refactored

--- 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
+++ 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
@@ -1,20 +1,38 @@
 package com.luv2code.tdd;
 
 public class FizzBuzz {
-    public static String compute(int i) {
-        // If number is divisible by 3, print Fizz
-        // If number is divisible by 5, print Buzz
-        // If number is divisible by 3 and 5, print fizzBuzz
-        // If number is not divisible by 3 or 5, print the number;
+    // If number is divisible by 3, print Fizz
+    // If number is divisible by 5, print Buzz
+    // If number is divisible by 3 and 5, print fizzBuzz
+    // If number is not divisible by 3 or 5, print the number;
 
-        if ((i % 3 == 0) && (i % 5 == 0)) {
-            return "FizzBuzz";
-        } else if (i % 3 == 0) {
-            return "Fizz";
-        } else if (i % 5 == 0) {
-            return "Buzz";
-        } else {
-            return Integer.toString(i);
+//    public static String compute(int i) {
+//        if ((i % 3 == 0) && (i % 5 == 0)) {
+//            return "FizzBuzz";
+//        } else if (i % 3 == 0) {
+//            return "Fizz";
+//        } else if (i % 5 == 0) {
+//            return "Buzz";
+//        } else {
+//            return Integer.toString(i);
+//        }
+//    }
+
+    public static String compute(int i) {
+        StringBuilder result = new StringBuilder();
+
+        if (i % 3 == 0) {
+            result.append("Fizz");
         }
+        if (i % 5 == 0) {
+            result.append("Buzz");
+        }
+
+        if(result.isEmpty()) {
+            result.append(i);
+        }
+
+        return result.toString();
     }
+
 }
