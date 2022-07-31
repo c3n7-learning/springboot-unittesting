31ed6ffc604fb24d95323515f458796c25b9780a
Timothy Karani <kartimothy@gmail.com>
2022-07-31 18:59:23 +0300

    Test passing for deleting

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/controller/GradebookController.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/controller/GradebookController.java
@@ -72,11 +72,21 @@ public class GradebookController {
             return "error";
         }
 
-
         studentService.configureStudentInformationModel(studentId, m);
-
 
         return "studentInformation";
     }
 
+    @GetMapping("/grades/{id}/{gradeType}")
+    public String deleteGrade(@PathVariable int id, @PathVariable String gradeType, Model m) {
+        int studentId = studentService.deleteGrade(id, gradeType);
+
+        if(studentId == 0) {
+            return "error";
+        }
+
+        studentService.configureStudentInformationModel(studentId, m);
+
+        return "studentInformation";
+    }
 }
